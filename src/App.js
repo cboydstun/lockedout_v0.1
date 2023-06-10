3395adbbeabb33f53a6fb4c48045676729cf3ed3
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-06-09 22:35:02 -0500

    init react and boilerplate

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './App.css'
+
+export default function App() {
+    return (
+        <div className='App'>
+            <h1>Coming Soon...</h1>
+            <img src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" alt="Coming Soon" />
+        </div>
+    )
+}
