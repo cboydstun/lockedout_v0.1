7e78da9ddb58554b7043757c4486b14777cdcd55
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-06-09 22:52:21 -0500

    add bad navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
+import Navigation from './components/Navigation'
+
 import './App.css'
 
 export default function App() {
     return (
         <div className='App'>
+            <Navigation />
             <h1>Coming Soon...</h1>
             <img src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" alt="Coming Soon" />
         </div>
---
+++ src/components/Navigation.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import './Navigation.css'
+
+export default function Navigation() {
+    return (
+        <nav role="navigation">
+            <div id="menuToggle">
+                <input type="checkbox" />
+                <span></span>
+                <span></span>
+                <span></span>
+                <ul id="menu">
+                    <a href="#"><li>Home</li></a>
+                    <a href="#"><li>About</li></a>
+                    <a href="#"><li>Info</li></a>
+                    <a href="#"><li>Contact</li></a>
+                    <a href="https://erikterwan.com/" target="_blank"><li>Show me more</li></a>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+
+
