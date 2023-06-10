a98d0e6d4fd3a0ec696ec0c6050ef174fd487cba
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-06-09 22:53:50 -0500

    got rid of bad navbar

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css'
 export default function App() {
     return (
         <div className='App'>
-            <Navigation />
+            {/* <Navigation /> */}
             <h1>Coming Soon...</h1>
             <img src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" alt="Coming Soon" />
         </div>
